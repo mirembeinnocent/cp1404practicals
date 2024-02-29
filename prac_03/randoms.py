19170f817649d525924e38f4a3a1dc620812a700
innocent_mirembe <mirembeinnocent88@gmail.com>
2024-02-29 13:17:10 +0300

    Add randoms program

---
+++ prac_03/randoms.py
@@ -0,0 +1,19 @@
+import random
+
+# 1.
+# A random integer between 5 and 20, including both endpoints.
+# The smallest number is 5, and the largest is 20.
+
+# 2.
+
+# A random integer between 3 and 9, including 3 but excluding 10 (since the step is 2).
+# The smallest number is 3, and the largest is 9.
+# No, because the step is 2, so only odd numbers are generated in this range.
+
+# 3.
+# A random floating-point number between 2.5 and 5.5.
+# The smallest number is 2.5, and the largest is 5.5.
+
+# 4. Write code to produce a random number between 1 and 100 inclusive.
+
+print(random.randint(1, 100))
